3ae5baf07d09560fd03f6ed2dfbd47d32beeb042
Barti <bartosz.majkowski@wp.pl>
2023-10-18 22:10:26 +0200

    early_stage_facebook_oauth2

--- src/main/java/autobids/apigateway/security/SecurityConfig.java
+++ src/main/java/autobids/apigateway/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package autobids.apigateway.security;
 
-import jakarta.servlet.http.Cookie;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,45 +8,22 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 
 
-import java.io.IOException;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    OAuthSuccessHandler oAuthSuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-//  TODO CORS              .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests((authorizeHttpRequests) ->
-                        authorizeHttpRequests
-                                .anyRequest()
-                                .authenticated())
-                .oauth2Login((oauth2Login) ->
-                        oauth2Login
-                                .successHandler(oAuthSuccessHandler)
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/cookies/cookielogout")
-                        .addLogoutHandler((request, response, auth) -> {
-                            for (Cookie cookie : request.getCookies()) {
-                                String cookieName = cookie.getName();
-                                Cookie cookieToDelete = new Cookie(cookieName, null);
-                                cookieToDelete.setMaxAge(0);
-                                try {
-                                    response.sendRedirect(System.getenv("FRONTEND_URI"));
-                                } catch (IOException e) {
-                                    throw new RuntimeException(e);
-                                }
-                                response.addCookie(cookieToDelete);
-                            }
-                        })
-                ).build()
-                ;
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(withDefaults())
+                .build();
     }
 }
