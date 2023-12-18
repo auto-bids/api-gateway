a0605aac8a814c8e313bf026b31cd6f4a8740f13
Barti <bartosz.majkowski@wp.pl>
2023-12-18 23:37:25 +0100

    added reactive get email from oauth

--- src/main/java/autobids/apigateway/ApiGatewayApplication.java
+++ src/main/java/autobids/apigateway/ApiGatewayApplication.java
@@ -2,13 +2,13 @@ package autobids.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 
+@EnableWebFlux
 @SpringBootApplication
 public class ApiGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
 }
