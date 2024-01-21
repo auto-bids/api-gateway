26e4439607e9241f3ba7f091c04b959c17b6e719
Barti <bartosz.majkowski@wp.pl>
2024-01-21 12:09:53 +0100

    authentication works and delete login endpoint

---
+++ src/main/java/autobids/apigateway/security/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,26 @@
+package autobids.apigateway.security;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@Component
+public class OAuth2LoginSuccessHandler implements ServerAuthenticationSuccessHandler {
+
+    @Override
+    public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
+        String redirectUrl = "http://localhost:3000";
+//        String redirectUrl = System.getenv("FRONTEND_URI");
+        ServerWebExchange serverWebExchange = webFilterExchange.getExchange();
+        serverWebExchange.getResponse().getHeaders().setLocation(URI.create(redirectUrl));
+        serverWebExchange.getResponse().setStatusCode(HttpStatus.SEE_OTHER);
+
+        return Mono.empty();
+    }
+}
--- src/main/java/autobids/apigateway/security/SecurityConfig.java
+++ src/main/java/autobids/apigateway/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package autobids.apigateway.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -14,25 +15,28 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Autowired
+    OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/profiles/create/me").authenticated()
                         .pathMatchers("/profiles/login/me").authenticated()
                         .pathMatchers("/profiles/delete/me").authenticated()
                         .pathMatchers("/profiles/edit/me").authenticated()
                         .anyExchange().permitAll()
                 )
-                .oauth2Login(withDefaults())
+                .oauth2Login((oauth2Login) ->
+                        oauth2Login
+                                .authenticationSuccessHandler(oAuth2LoginSuccessHandler)
+                )
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .logout(logout -> logout
                         .logoutUrl("/logout")
@@ -47,7 +51,8 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(List.of(System.getenv("PROFILES_URI")));
+        corsConfig.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4000", "http://localhost:4100", "https://dev-wgrfncmy5tahnvay.us.auth0.com"));
+        //        corsConfig.setAllowedOrigins(List.of(System.getenv("FRONTEND_URI"), System.getenv("PROFILES_URI"), System.getenv("OAUTH_URI")));
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowCredentials(true);
         corsConfig.addAllowedMethod("*");
