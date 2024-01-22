ba457df2f011c2dd087f663c8472fa5850f6031f
Barti <bartosz.majkowski@wp.pl>
2024-01-23 00:36:52 +0100

    added cars routes

--- src/main/java/autobids/apigateway/routes/Routes.java
+++ src/main/java/autobids/apigateway/routes/Routes.java
@@ -1,8 +1,10 @@
 package autobids.apigateway.routes;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
@@ -12,8 +14,148 @@ import org.springframework.stereotype.Component;
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.addOriginalRequestUrl;
 
+
 @Component
-public class CarsRoutes {
+public class Routes {
+
+
+    @Bean
+    public RouteLocator profileRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("/profiles/user/**", r -> r
+                        .path("/profiles/user/**")
+                        .uri(System.getenv("PROFILES_URI")))
+                .build();
+    }
+
+
+    @Bean
+    public RouteLocator createRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("/profiles/login/me", r -> r
+                        .method("GET")
+                        .and()
+                        .path("/profiles/login/me")
+                        .filters(f -> f
+                                .setPath("/profiles/user")
+                                .modifyRequestBody(String.class, Profile.class, MediaType.APPLICATION_JSON_VALUE,
+                                        (exchange, s) -> ReactiveSecurityContextHolder.getContext()
+                                                .map(SecurityContext::getAuthentication)
+                                                .map(authentication -> new Profile(
+                                                        ((OAuth2User) authentication.getPrincipal()).getAttribute("name"),
+                                                        ((OAuth2User) authentication.getPrincipal()).getAttribute("email"),
+                                                        "https://pbs.twimg.com/profile_images/1151437589062266880/AuZyoH2__400x400.jpg"
+                                                ))
+                                )
+                        )
+                        .uri(System.getenv("PROFILES_URI"))
+                ).build();
+    }
+
+
+    @Bean
+    public RouteLocator editRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("/profiles/edit/me", r -> r
+                        .method("PUT")
+                        .and()
+                        .path("/profiles/edit/me")
+                        .filters(f -> f
+                                .filter((exchange, chain) ->
+                                        ReactiveSecurityContextHolder.getContext()
+                                                .map(SecurityContext::getAuthentication)
+                                                .map(authentication -> {
+                                                    OAuth2User user = (OAuth2User) authentication.getPrincipal();
+                                                    ServerHttpRequest req = exchange.getRequest();
+                                                    addOriginalRequestUrl(exchange, req.getURI());
+                                                    String path = req.getURI().getRawPath();
+                                                    String newPath = path.replaceAll(
+                                                            "/profiles/edit/me",
+                                                            "/profiles/user/" + user.getAttribute("email"));
+                                                    exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, newPath);
+                                                    ServerHttpRequest request = req.mutate().path(newPath).build();
+                                                    return exchange.mutate().request(request).build();
+                                                })
+                                                .flatMap(chain::filter)
+                                )
+                        )
+                        .uri(System.getenv("PROFILES_URI"))
+                ).build();
+    }
+
+
+    @Bean
+    public RouteLocator deleteSecuredProfileRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("/profiles/delete/me", r -> r
+                        .method("DELETE")
+                        .and()
+                        .path("/profiles/delete/me")
+                        .filters(f -> f
+                                .filter((exchange, chain) ->
+                                        ReactiveSecurityContextHolder.getContext()
+                                                .map(SecurityContext::getAuthentication)
+                                                .map(authentication -> {
+                                                    OAuth2User user = (OAuth2User) authentication.getPrincipal();
+                                                    ServerHttpRequest req = exchange.getRequest();
+                                                    addOriginalRequestUrl(exchange, req.getURI());
+                                                    String path = req.getURI().getRawPath();
+                                                    String newPath = path.replaceAll(
+                                                            "/profiles/delete/me",
+                                                            "/profiles/user/" + user.getAttribute("email"));
+                                                    exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, newPath);
+                                                    ServerHttpRequest request = req.mutate().path(newPath).build();
+                                                    return exchange.mutate().request(request).build();
+                                                })
+                                                .flatMap(chain::filter)
+                                )
+                        )
+                        .uri(System.getenv("PROFILES_URI"))
+                ).build();
+    }
+
+
+    private static class Profile {
+        private String userName;
+        private String email;
+        private String profileImage;
+
+        public Profile(String userName, String email, String profileImage) {
+            this.userName = userName;
+            this.email = email;
+            this.profileImage = profileImage;
+        }
+
+        @JsonGetter("user_name")
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        @JsonGetter("email")
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        @JsonGetter("profile_image")
+        public String getProfileImage() {
+            return profileImage;
+        }
+
+        public void setProfileImage(String profileImage) {
+            this.profileImage = profileImage;
+        }
+    }
+
+
+
     @Bean
     public RouteLocator carsRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
