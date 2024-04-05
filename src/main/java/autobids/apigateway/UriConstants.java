443f866ff8bc8d51e500351bc78ca62931e336eb
Barti <bartosz.majkowski@wp.pl>
2024-04-06 00:31:13 +0200

    motorcycles routes added

--- src/main/java/autobids/apigateway/UriConstants.java
+++ src/main/java/autobids/apigateway/UriConstants.java
@@ -3,6 +3,7 @@ package autobids.apigateway;
 public final class UriConstants {
     public static final String ADMIN_ROUTES_PROFILES = "/admin/profiles/**";
     public static final String ADMIN_ROUTES_CARS = "/admin/cars/**";
+    public static final String ADMIN_ROUTES_MOTORCYCLES = "/admin/motorcycles/**";
 
     public static final String PROFILES_USER = "/profiles/user/**";
     public static final String PROFILES_USER_ROUTES = "/profiles/user/{email}";
@@ -18,4 +19,12 @@ public final class UriConstants {
     public static final String CARS_SEARCH_USER = "/cars/search/user/{email}/{page}";
     public static final String CARS_SEARCH_ME = "/cars/search/user/me/**";
     public static final String CARS_SEARCH_ME_ROUTES = "/cars/search/user/me/{page}";
+    public static final String MOTORCYCLES_SEARCH = "/motorcycles/search/{page}";
+    public static final String MOTORCYCLES_DETAILS = "/motorcycles/details/{id}";
+    public static final String MOTORCYCLES_ADD = "/motorcycles/add/me";
+    public static final String MOTORCYCLES_DELETE = "/motorcycles/delete/me";
+    public static final String MOTORCYCLES_DELETE_ALL = "/motorcycles/delete/all/me";
+    public static final String MOTORCYCLES_EDIT = "/motorcycles/edit/me";
+    public static final String MOTORCYCLES_SEARCH_ME_ROUTES = "/motorcycles/search/user/me/{page}";
+    public static final String MOTORCYCLES_SEARCH_USER = "/motorcycles/search/user/{email}/{page}";
 }
